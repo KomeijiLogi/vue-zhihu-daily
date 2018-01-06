78d9b3032f578ae0d8a3b291104672ecafdcabf9
KomeijiLogi <zclxuan2012@163.com>
2018-01-06 16:05:27 +0800

    first commit

---
+++ src/vuex/mutations-types.js
@@ -0,0 +1,20 @@
+//定义types
+
+//刷新loading
+export const SET_LOADING='SET_LOADING';
+
+//获取数据
+export const GET_DATAS='GET_DATAS';
+
+
+//新增新闻
+export const ADD_NEWS='ADD_NEWS';
+
+//第一次加载时刷新新闻
+export const REFRESH_NEWS='REFRESH_NEWS';
+
+//刷新首页新闻内容
+export  const CHANGE_NEWS='CHANGE_NEWS';
+
+
+
