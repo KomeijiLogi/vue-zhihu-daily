a14deef1670c3acf8b98c02f2a1525f7b850cf6d
KomeijiLogi <zclxuan2012@163.com>
2018-01-09 16:35:13 +0800

    更新内容，为详情页准备

--- src/components/detail/main.vue
+++ src/components/detail/main.vue
@@ -1,10 +1,30 @@
 <template>
+    <div class="detail">
+       <div v-if="this.$store.state.newsType===0" class="top-wrapper">
 
+       </div>
+    </div>
 </template>
 
 <script>
     export default {
+       data(){
+          return{
+            
+          }
+       },
+       methods:{
+         fetchData(){
+            let id =this.$route.params.id;
+            this.$axios.get('/api/news/'+id)
+              .then((res)=>{
 
+              })
+              .catch((err)=>{
+                 console.log(err);
+              });
+         }
+       }
     }
 </script>
 
