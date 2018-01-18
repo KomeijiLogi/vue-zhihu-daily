94fe062bbb940eed1632cbefcb3469a44c8292fe
KomeijiLogi <zclxuan2012@163.com>
2018-01-18 15:55:28 +0800

    消息页前置

--- src/components/message/messagemain.vue
+++ src/components/message/messagemain.vue
@@ -1,20 +1,34 @@
 <template>
     <div class="messagemain">
        <span>通知列表</span>
-       
+       <section class="notice-area">
+           <div class="notice-area-true" v-if="hasNotice">
+              
+           </div>
+           <div class="notice-area-false" v-else>
+              <span>没有内容</span>
+           </div>
+       </section>
     </div>
 </template>
 
 <script>
     export default {
         //name: "messagemain"
+        data(){
+           return{
+              hasNotice:true   //是否有通知内容
+           }
+        }
     }
 </script>
 
 <style lang="less" scoped>
-
+  @import "../../assets/fz";
+  @import "../../assets/icons/iconfont";
   .messagemain{
+     span{
 
-
+     }
   }
 </style>
