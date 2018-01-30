b3899324d14c16aca1c66e19b4f6c84de35d5a44
KomeijiLogi <zclxuan2012@163.com>
2018-01-30 14:24:12 +0800

    更新内容

--- src/http/api.js
+++ src/http/api.js
@@ -1,8 +1,17 @@
 import axios from 'axios'
 
 
+const api=axios.create({
+   baseURL:'http://news-at.zhihu.com',
+   timeout:5000,
+   headers:{
+      // 'Content-Type':'application/x-www-form-urlencoded',
+      'X-Requested-With':'XMLHttpRequest',
+     'Access-Control-Allow-Origin':'http://news-at.zhihu.com',
+     'Access-Control-Allow-Credentials':true,     //是否允许发送cookies,但是需要服务器端和ajax中设置,现在这么配置依然不会处理cookies
 
-
+   }
+})
 
 
 
